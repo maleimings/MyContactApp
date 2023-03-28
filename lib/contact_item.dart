7cd82b9574e2962c260903627ed1f44c2bd255e0
Maleiming <maleimings@gmail.com>
2023-03-28 17:57:27 +0800

    Add drawer

---
+++ lib/contact_item.dart
@@ -0,0 +1,23 @@
+class ContactItem {
+  final int id;
+  final String name;
+  final String cellphone;
+  final String telephone;
+  final bool favorite;
+
+  ContactItem( {required this.id, required this.name, required this.cellphone, required this.telephone, required this.favorite} );
+
+  factory ContactItem.fromJson(Map<String, dynamic> json) {
+    return ContactItem(id: json['id'], name: json['name'], cellphone: json['cellphone'], telephone: json['telephone'], favorite: json['favorite']);
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'cellphone': cellphone,
+      'telephone': telephone,
+      'isFavorite': favorite ? 1 : 0
+    };
+  }
+}
---
+++ lib/database_manager.dart
@@ -0,0 +1,37 @@
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:async/async.dart';
+
+class DatabaseManager {
+  static final DatabaseManager _databaseManager = DatabaseManager._internal();
+
+  DatabaseManager._internal();
+
+  static DatabaseManager get instance => _databaseManager;
+
+  static Database? _database;
+
+  final _initDatabaseMemoizer = AsyncMemoizer<Database>();
+
+  Future<Database> get database async {
+    if (_database != null) {
+      return _database!;
+    }
+
+    _database = await _initDatabaseMemoizer.runOnce(() async {
+      return await _initDatabase();
+    });
+
+    return _database!;
+  }
+
+  Future<Database> _initDatabase() async {
+    return openDatabase(
+      join(await getDatabasesPath(), 'my_contact.db'),
+      onCreate: (db, version) {
+        return db.execute('CREATE TABLE contact(id INTEGER PRIMARY KEY, name TEXT, cellphone TEXT, telephone TEXT,  favorite INTEGER)');
+      },
+      version: 1,
+    );
+  }
+}
