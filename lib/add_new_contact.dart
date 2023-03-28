343ac2653b2a3dd3d0c4b69edf4eb87e0047ed95
Maleiming <maleimings@gmail.com>
2023-03-28 20:32:33 +0800

    Fix overflow issue

--- lib/add_new_contact.dart
+++ lib/add_new_contact.dart
@@ -6,96 +6,102 @@ class AddNewContact extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final TextEditingController _nameInputController = TextEditingController();
-    final TextEditingController _cellphoneInputController = TextEditingController();
-    final TextEditingController _telephoneInputController = TextEditingController();
+    final TextEditingController _cellphoneInputController =
+        TextEditingController();
+    final TextEditingController _telephoneInputController =
+        TextEditingController();
 
     // TODO: implement build
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Text('Add New Contact'),
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back_ios),
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text('Add New Contact'),
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back_ios),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.star),
+              onPressed: () {},
+            )
+          ],
         ),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.star),
-            onPressed: () {},
-          )
-        ],
-      ),
-      body: Column(
-        children: [
-          const Padding(padding: EdgeInsets.only(top: 60)),
-          Center(
-            child: Container(
-              width: 80,
-              height: 80,
-              alignment: Alignment.center,
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: Colors.black,
-                ),
-                shape: BoxShape.circle,
-              ),
-              child: Center(
-                  child: SizedBox(
-                height: 80,
-                width: 80,
-                child: IconButton(
-                  icon: const Icon(
-                    Icons.photo_camera,
-                    size: 50,
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              const Padding(padding: EdgeInsets.only(top: 60)),
+              Center(
+                child: Container(
+                  width: 80,
+                  height: 80,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.black,
+                    ),
+                    shape: BoxShape.circle,
                   ),
-                  onPressed: () {},
+                  child: Center(
+                      child: SizedBox(
+                    height: 80,
+                    width: 80,
+                    child: IconButton(
+                      icon: const Icon(
+                        Icons.photo_camera,
+                        size: 50,
+                      ),
+                      onPressed: () {},
+                    ),
+                  )),
                 ),
-              )),
-            ),
-          ),
-          Padding(padding: const EdgeInsets.only(top: 80, right:20, left: 20),
-            child: TextField(
-              controller: _nameInputController,
-              keyboardType: TextInputType.text,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                contentPadding: EdgeInsets.all(20.0),
-                icon: Icon(Icons.person),
-                labelText: 'Full Name',
               ),
-              autofocus: false,
-            ),
-          ),
-          Padding(padding: const EdgeInsets.only(top:20, right:20, left: 20),
-            child: TextField(
-              controller: _cellphoneInputController,
-              keyboardType: TextInputType.phone,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                contentPadding: EdgeInsets.all(20.0),
-                icon: Icon(Icons.phone_android),
-                labelText: 'Cellphone Number',
+              Padding(
+                padding: const EdgeInsets.only(top: 80, right: 20, left: 20),
+                child: TextField(
+                  controller: _nameInputController,
+                  keyboardType: TextInputType.text,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    contentPadding: EdgeInsets.all(20.0),
+                    icon: Icon(Icons.person),
+                    labelText: 'Full Name',
+                  ),
+                  autofocus: false,
+                ),
               ),
-              autofocus: false,
-            ),
-          ),
-          Padding(padding: const EdgeInsets.only(top:20, right:20, left: 20),
-            child: TextField(
-              controller: _telephoneInputController,
-              keyboardType: TextInputType.phone,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                contentPadding: EdgeInsets.all(20.0),
-                icon: Icon(Icons.phone),
-                labelText: 'Telephone Number',
+              Padding(
+                padding: const EdgeInsets.only(top: 20, right: 20, left: 20),
+                child: TextField(
+                  controller: _cellphoneInputController,
+                  keyboardType: TextInputType.phone,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    contentPadding: EdgeInsets.all(20.0),
+                    icon: Icon(Icons.phone_android),
+                    labelText: 'Cellphone Number',
+                  ),
+                  autofocus: false,
+                ),
               ),
-              autofocus: false,
-            ),
-          )
-        ],
-      ),
-    );
+              Padding(
+                padding: const EdgeInsets.only(top: 20, right: 20, left: 20),
+                child: TextField(
+                  controller: _telephoneInputController,
+                  keyboardType: TextInputType.phone,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    contentPadding: EdgeInsets.all(20.0),
+                    icon: Icon(Icons.phone),
+                    labelText: 'Telephone Number',
+                  ),
+                  autofocus: false,
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }
